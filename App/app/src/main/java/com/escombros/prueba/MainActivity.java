c3c6459ad553341c6c494310910f8db0a1a42051
searleser97 <serchgabriel97@gmail.com>
2018-09-09 08:22:29 -0500

    updated: 09/09/2018 08:22:29 AM

---
+++ App/app/src/main/java/com/escombros/prueba/MainActivity.java
@@ -0,0 +1,207 @@
+package com.escombros.prueba;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothSPP bluetooth;
+
+    Button connect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        bluetooth = new BluetoothSPP(this);
+
+        connect = findViewById(R.id.connect);
+
+        if (!bluetooth.isBluetoothAvailable()) {
+            Toast.makeText(getApplicationContext(), "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bluetooth.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                System.out.println("mensaje: " + message);
+                int intensidad = Integer.parseInt(message);
+                if (intensidad > 3) {
+                    try {
+                        Date date= new Date();
+                        long time = date.getTime();
+                        Timestamp ts = new Timestamp(time);
+                        String jsobject = "{id_sensor : 1, timestamp : " + ts.toString() + " ," +
+                                "location: {x: 19.4510848, y : -99.1289344, magnitud : " + intensidad + "} }";
+                        // las coordenadas de arriba son estaticas por flojera XD
+                        System.out.println(jsobject);
+                        JSONObject jsonObject = new JSONObject(jsobject);
+                        sendHttpRequest("201.166.145.191:80", jsonObject);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    raiseAudio();
+                    showNotification("Alerta", "Se aproxima un sismo, ¡Tome precauciones!", getApplicationContext());
+                }
+            }
+        });
+
+        bluetooth.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() {
+            public void onDeviceConnected(String name, String address) {
+                connect.setText("Connected to " + name);
+            }
+
+            public void onDeviceDisconnected() {
+                connect.setText("Connection lost");
+            }
+
+            public void onDeviceConnectionFailed() {
+                connect.setText("Unable to connect");
+            }
+        });
+
+
+
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetooth.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bluetooth.disconnect();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (!bluetooth.isBluetoothEnabled()) {
+            bluetooth.enable();
+        } else {
+            if (!bluetooth.isServiceAvailable()) {
+                bluetooth.setupService();
+                bluetooth.startService(BluetoothState.DEVICE_OTHER);
+            }
+        }
+    }
+
+
+    public void onDestroy() {
+        super.onDestroy();
+        bluetooth.stopService();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bluetooth.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bluetooth.setupService();
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    public void raiseAudio() {
+        AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVolume = audio.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION);
+        float percent = 1f;
+        int seventyVolume = (int) (maxVolume*percent);
+        audio.setStreamVolume(AudioManager.STREAM_NOTIFICATION, seventyVolume, 0);
+    }
+
+    public void showNotification(String title, String message, Context mContext)
+    {
+        //Creates an explicit intent for an Activity in your app*
+        Intent resultIntent = new Intent(mContext , MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(mContext,
+                0 , resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        mBuilder.setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(false)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel("3", "prueba", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            assert mNotificationManager != null;
+            mBuilder.setChannelId("3");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        assert mNotificationManager != null;
+        raiseAudio();
+        mNotificationManager.notify(0 , mBuilder.build());
+    }
+
+    public void sendHttpRequest(String url, JSONObject jsonObject) {
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.toString());
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+//        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+}
